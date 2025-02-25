4bb6cfadb40ec9ff0bce0ef38ef05cf7bf4153dd
Ardhi Lukianto <ardhi@lukianto.com>
2025-02-25 22:08:21 +0700

    Updates

--- bajo/method/attachment/copy-uploaded.js
+++ bajo/method/attachment/copy-uploaded.js
@@ -1,17 +1,20 @@
 import path from 'path'
 
-async function copyUploaded (name, id, { req, setField, setFile, mimeType, stats, silent = true } = {}) {
+async function copyUploaded (name, id, options = {}) {
   const { fs } = this.app.bajo.lib
+  const { omit } = this.app.bajo.lib._
+  console.log(omit(options, ['req']))
+  const { req, setField, setFile, mimeType, stats, silent = true } = options
   name = this.attachmentPreCheck(name)
   if (!name) {
     if (silent) return
     throw this.error('isMissing%s', this.print.write('field.name'))
   }
-  if (!this.bajoWeb) {
+  if (!this.app.waibu) {
     if (silent) return
-    throw this.error('missingPlugin%s')
+    throw this.error('missingPlugin%s', 'Waibu')
   }
-  const { dir, files } = await this.bajoWeb.getUploadedFiles(req.id, false, true)
+  const { dir, files } = await this.app.waibu.getUploadedFiles(req.id, false, true)
   const result = []
   if (files.length === 0) return result
   for (const f of files) {
@@ -20,6 +23,7 @@ async function copyUploaded (name, id, { req, setField, setFile, mimeType, stats
     field = setField ?? field
     const file = setFile ?? parts.join('@')
     const opts = { source: f, field, file, mimeType, stats, req }
+    console.log(setField, setFile, omit(opts, ['req']))
     const rec = await this.attachmentCreate(name, id, opts)
     delete rec.dir
     result.push(rec)
---
+++ waibuMpa/route/attachment/@model/@id/@field/@file.js
@@ -0,0 +1,16 @@
+import path from 'path'
+
+async function attachment (req, reply) {
+  const { importPkg, getPluginDataDir, pascalCase } = this.app.bajo
+  const mime = await importPkg('waibu:mime')
+  const { fs } = this.app.bajo.lib
+  const file = `${getPluginDataDir('dobo')}/attachment/${pascalCase(req.params.model)}/${req.params.id}/${req.params.field}/${req.params.file}`
+  if (!fs.existsSync(file)) throw this.error('_notFound')
+  const mimeType = mime.getType(path.extname(file))
+  reply.header('Content-Type', mimeType)
+  const stream = fs.createReadStream(file)
+  reply.send(stream)
+  return reply
+}
+
+export default attachment
