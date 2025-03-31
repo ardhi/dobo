b97067e6ba9b742e14d7393f5c4d091f549ef8bf
Ardhi Lukianto <ardhi@lukianto.com>
2025-03-31 10:02:56 +0700

    Updates

--- waibuMpa/route/attachment/@model/@id/@field/@file.js
+++ waibuMpa/route/attachment/@model/@id/@field/@file.js
@@ -1,20 +1,24 @@
 import path from 'path'
 
 async function attachment (req, reply) {
-  const { isString } = this.lib._
+  const { isString, isEmpty } = this.lib._
   const { importPkg, getPluginDataDir, pascalCase } = this.app.bajo
   const { routePath } = this.app.waibu
   const mime = await importPkg('waibu:mime')
-  const { fs } = this.lib
-  const file = `${getPluginDataDir('dobo')}/attachment/${pascalCase(req.params.model)}/${req.params.id}/${req.params.field}/${req.params.file}`
-  const mimeType = mime.getType(path.extname(file))
+  const { fs, fastGlob } = this.lib
+  let file = `${getPluginDataDir('dobo')}/attachment/${pascalCase(req.params.model)}/${req.params.id}/${req.params.field}/${req.params.file}`
+  if (path.basename(file) === '_first') {
+    const files = await fastGlob(`${path.dirname(file)}/*`)
+    if (files.length > 0) file = files[0]
+  }
+  const mimeType = mime.getType(path.extname(file)) ?? ''
   if (!fs.existsSync(file)) {
     if (!req.query.notfound) throw this.error('_notFound', { noContent: true })
     const [, ext] = mimeType.split('/')
-    const replacer = isString(req.query.notfound) ? req.query.notfound : `waibuStatic.asset:/not-found.${ext}`
+    const replacer = isString(req.query.notfound) ? req.query.notfound : `waibuStatic.asset:/not-found.${ext ?? 'png'}`
     return reply.redirectTo(routePath(replacer))
   }
-  reply.header('Content-Type', mimeType)
+  if (!isEmpty(mimeType)) reply.header('Content-Type', mimeType)
   const stream = fs.createReadStream(file)
   reply.send(stream)
   return reply
